0d0806661fbb4dc484d689fc300adb9a3d98a0ee
mercerco <reneesarno@gmail.com>
2025-09-13 20:55:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * 1) EDIT_SHEET_URL: the editable sheet link (Share -> Anyone with the link can EDIT)
  * 2) PUBLISHED_CSV_URL: File -> Share -> Publish to web -> CSV link (read-only)
  */
-const EDIT_SHEET_URL = "PASTE_YOUR_EDITABLE_GOOGLE_SHEET_LINK_HERE";
+const EDIT_SHEET_URL = "https://docs.google.com/spreadsheets/d/1FCNg_wnD77gZE-J23OX0OD5cIlQA8sc0EdYmYHn6i6U/edit?gid=0#gid=0";
 const PUBLISHED_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTXRa9t1KwcU64FC4DxcUgyiQdN2G-XCXnrvIjKQicoRwrUFCCyi0E7B8_GjaA-dkjFdhiZ_1n7xjbf/pub?output=csv";
 
 // Optional display timezone (IANA). Leave blank to use viewer's local timezone.
